26ab119ecdeb1da236e12d1487df67a9671fbe45
fexxix <fexxix@gmail.com>
2024-01-25 16:03:35 +0500

    add: added /boards protected page for authed users

---
+++ src/app/boards/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import { signOut, auth } from "../../../auth"
+import { redirect } from "next/navigation"
+
+export default async function () {
+  const session = await auth()
+
+  if (!session) {
+    redirect("/login")
+  }
+
+  return (
+    <div
+      className={cn("flex flex-col justify-center items-center gap-10 h-full")}
+    >
+      <form
+        action={async () => {
+          "use server"
+          await signOut()
+        }}
+      >
+        <Button>SignOut</Button>
+      </form>
+    </div>
+  )
+}
